4fa5ddfd528b376ffc98f380c613ef5bf82699be
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-05-09 10:56:24 -0300

    exemplos do capitulo 1

---
+++ pythonProject/Capitulo 1/exemplo1interpretador.py
@@ -0,0 +1,2 @@
+# O interpretador do Python é um programa que lê e executa um código python.
+
---
+++ pythonProject/Capitulo 1/exemplo3valoresetipos.py
@@ -0,0 +1,20 @@
+# Tipos de Dados
+# Inteiros 12345... positivos e negativos
+# Ponto flutuante 1.0, 1.3, 5.153269 ... positivos e negativos
+# Caracteres que são apenas um simbolo
+# Strings que são conjuntos de caracteres
+
+# Exemplo de uma variavel com numero inteiro
+num1 = 7
+
+# Exemplo de uma variavel com numero de ponto flutuante
+num2 = 3.1457
+
+# Exemplo de uma variavel com um caractere
+char1 = 'a'
+
+# Exemplo de uma variavel com uma string
+string1 = 'jose'
+
+# Comando para descobri quel é o tipo de variavel
+print(type(num2))
