7ceb9b40aa21017c6287d074b8342a32499da985
Lucas <ana.santos.ribeiro01@escola.pr.gov.br>
2025-05-16 09:55:24 -0300

    Exercicio 3 metade

--- pythonProject/Capitulo 2/Exercicio3.py
+++ pythonProject/Capitulo 2/Exercicio3.py
@@ -1,2 +1,21 @@
 #primeiro horario 6:52 e corre 1 quilometro (8min15s por quilometro), dps de 3 quilometros (7min12s) e 1 quilometro  usa o passo em primeiro lugr que hora chegar por cafe da manha
 #quilometro 1 era 7:00 e 15 segundos, no quilometro 4 7:21,51
+
+print("Tempo do primeiro trecho: 8min e 15seg")
+minutosPrimeirotrecho = 8
+segundoPrimeirotrecho = 15
+totalTempoprimeirotrecho = (8*60) + 15
+print("Tempo do Primeiro techo em segundos",totalTempoprimeirotrecho)
+
+print("Tempo do segundo trecho: 7min e 12seg")
+minutosSegundotrecho = 7
+segundoSegundotrecho = 12
+totalTempoSegundotrecho = (7*60) + 12
+print("Tempo do Segundo techo em segundos",totalTempoSegundotrecho)
+
+print("Tempo do Terceiro trecho: 8min e 15seg")
+minutosTerceirotrecho = 8
+segundoTerceirotrecho = 15
+totalTempoTerceirotrecho = (8*60) + 15
+print("Tempo do Terceiro techo em segundos",totalTempoTerceirotrecho)
+
